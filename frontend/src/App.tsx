b9f5014abcaec51bc0fb4c03dc00340664308754
Xiaochuan Xu <xiaochuanxu@cmu.edu>
2022-01-27 17:29:19 -0500

    code styles change & delete unnecessary codes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
-import { Component } from 'react';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
 import Handlebars from "handlebars";
 
@@ -20,57 +20,75 @@ const template = Handlebars.compile(html);
 var oldHref = "http://localhost:3000";
 
 interface Cell {
-  text: String,
-  clazz: String,
-  link: String
+  text: String;
+  clazz: String;
+  link: String;
 }
 
 interface Cells {
-  cells: Array<Cell>
+  cells: Array<Cell>;
 }
 
 class App extends Component<{}, Cells> {
-  constructor({}) {
+  constructor() {
     super({});
     this.state = {
-      cells: [{ text: "", clazz: "playable", link: "/play?x=0&y=0"},{ text: "", clazz: "playable", link: "/play?x=1&y=0"},{ text: "", clazz: "playable", link: "/play?x=2&y=0"},
-              { text: "", clazz: "playable", link: "/play?x=0&y=1"},{ text: "", clazz: "playable", link: "/play?x=1&y=1"},{ text: "", clazz: "playable", link: "/play?x=2&y=1"},
-              { text: "", clazz: "playable", link: "/play?x=0&y=2"},{ text: "", clazz: "playable", link: "/play?x=1&y=2"},{ text: "", clazz: "playable", link: "/play?x=2&y=2"}]
-    }
+      cells: [
+        { text: "", clazz: "playable", link: "/play?x=0&y=0" },
+        { text: "", clazz: "playable", link: "/play?x=1&y=0" },
+        { text: "", clazz: "playable", link: "/play?x=2&y=0" },
+        { text: "", clazz: "playable", link: "/play?x=0&y=1" },
+        { text: "", clazz: "playable", link: "/play?x=1&y=1" },
+        { text: "", clazz: "playable", link: "/play?x=2&y=1" },
+        { text: "", clazz: "playable", link: "/play?x=0&y=2" },
+        { text: "", clazz: "playable", link: "/play?x=1&y=2" },
+        { text: "", clazz: "playable", link: "/play?x=2&y=2" },
+      ],
+    };
   }
 
   convertToCell(p: any): Array<Cell> {
     const newCells: Array<Cell> = [];
-    for (var i=0; i < p["cells"].length; i++) {
-      var c: Cell = {text: p["cells"][i]["text"], clazz: p["cells"][i]["clazz"], link: p["cells"][i]["link"]}
+    for (var i = 0; i < p["cells"].length; i++) {
+      var c: Cell = {
+        text: p["cells"][i]["text"],
+        clazz: p["cells"][i]["clazz"],
+        link: p["cells"][i]["link"],
+      };
       newCells.push(c);
     }
 
     return newCells;
-  };
+  }
 
   async newGame() {
-    const response = await fetch('newgame');
+    const response = await fetch("newgame");
     const json = await response.json();
 
     const newCells: Array<Cell> = this.convertToCell(json);
-    this.setState({cells: newCells});
-  };
+    this.setState({ cells: newCells });
+  }
 
   async play(url: String) {
-    const href = "play?" + url.split('?')[1];
-    const response = await fetch(href)
+    const href = "play?" + url.split("?")[1];
+    const response = await fetch(href);
     const json = await response.json();
 
     const newCells: Array<Cell> = this.convertToCell(json);
-    this.setState({cells: newCells});
+    this.setState({ cells: newCells });
   }
 
   async move() {
-    if (window.location.href == "http://localhost:3000/newgame" && oldHref != window.location.href) {
+    if (
+      window.location.href === "http://localhost:3000/newgame" &&
+      oldHref !== window.location.href
+    ) {
       this.newGame();
       oldHref = window.location.href;
-    } else if (window.location.href.split('?')[0] == "http://localhost:3000/play" && oldHref != window.location.href) {
+    } else if (
+      window.location.href.split("?")[0] === "http://localhost:3000/play" &&
+      oldHref !== window.location.href
+    ) {
       this.play(window.location.href);
       oldHref = window.location.href;
     }
@@ -80,7 +98,11 @@ class App extends Component<{}, Cells> {
     this.move();
     return (
       <div className="App">
-        <div dangerouslySetInnerHTML={{ __html: template({cells: this.state.cells}) }} />
+        <div
+          dangerouslySetInnerHTML={{
+            __html: template({ cells: this.state.cells }),
+          }}
+        />
       </div>
     );
   }
