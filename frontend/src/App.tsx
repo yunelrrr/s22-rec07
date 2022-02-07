6781aaf9d083c12f307561177391a59993f12d18
ibasow1015 <ibasow1015@gmail.com>
2022-02-07 10:25:15 -0500

    Add change to how handlebars is loaded

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,8 @@
-import { Component } from 'react';
-import './App.css';
+import Handlebars from "handlebars"
+import { Component } from 'react'
+import './App.css'
 
-import Handlebars from "handlebars";
-
-const html = `<div id="board">
-                {{#each cells}}\
-                  {{#if link}}\
-                    <a href={{link}}><div class="cell {{clazz}}">{{text}}</div></a>\
-                  {{else}}\
-                    <div class="cell {{clazz}}">{{text}}</div>\
-                  {{/if}}\
-                {{/each}}\
-              </div>\
-              <div id="bottombar">\
-                <a href="/newgame"><button>New Game</button></a>\
-                <button>Undo</button>\
-              </div>`;
-const template = Handlebars.compile(html);
-var oldHref = "http://localhost:3000";
+var oldHref = "http://localhost:3000"
 
 interface Cell {
   text: String,
@@ -26,23 +11,33 @@ interface Cell {
 }
 
 interface Cells {
-  cells: Array<Cell>
+  cells: Array<Cell>,
+  template: HandlebarsTemplateDelegate<any>
 }
 
-class App extends Component<{}, Cells> {
-  constructor({}) {
-    super({});
+interface Props {
+}
+
+class App extends Component<Props, Cells> {
+  constructor(props: Props) {
+    super(props);
     this.state = {
       cells: [{ text: "", clazz: "playable", link: "/play?x=0&y=0"},{ text: "", clazz: "playable", link: "/play?x=1&y=0"},{ text: "", clazz: "playable", link: "/play?x=2&y=0"},
               { text: "", clazz: "playable", link: "/play?x=0&y=1"},{ text: "", clazz: "playable", link: "/play?x=1&y=1"},{ text: "", clazz: "playable", link: "/play?x=2&y=1"},
-              { text: "", clazz: "playable", link: "/play?x=0&y=2"},{ text: "", clazz: "playable", link: "/play?x=1&y=2"},{ text: "", clazz: "playable", link: "/play?x=2&y=2"}]
-    }
+              { text: "", clazz: "playable", link: "/play?x=0&y=2"},{ text: "", clazz: "playable", link: "/play?x=1&y=2"},{ text: "", clazz: "playable", link: "/play?x=2&y=2"}],
+      template: this.loadTemplate()
+    };
+  }
+
+  loadTemplate (): HandlebarsTemplateDelegate<any> {
+    const src = document.getElementById("handlebars");
+    return Handlebars.compile(src?.innerHTML, {});
   }
 
   convertToCell(p: any): Array<Cell> {
     const newCells: Array<Cell> = [];
     for (var i=0; i < p["cells"].length; i++) {
-      var c: Cell = {text: p["cells"][i]["text"], clazz: p["cells"][i]["clazz"], link: p["cells"][i]["link"]}
+      var c: Cell = {text: p["cells"][i]["text"], clazz: p["cells"][i]["clazz"], link: p["cells"][i]["link"]};
       newCells.push(c);
     }
 
@@ -59,31 +54,31 @@ class App extends Component<{}, Cells> {
 
   async play(url: String) {
     const href = "play?" + url.split('?')[1];
-    const response = await fetch(href)
+    const response = await fetch(href);
     const json = await response.json();
 
     const newCells: Array<Cell> = this.convertToCell(json);
     this.setState({cells: newCells});
-  }
+  };
 
   async move() {
-    if (window.location.href == "http://localhost:3000/newgame" && oldHref != window.location.href) {
+    if (window.location.href === "http://localhost:3000/newgame" && oldHref !== window.location.href) {
       this.newGame();
       oldHref = window.location.href;
-    } else if (window.location.href.split('?')[0] == "http://localhost:3000/play" && oldHref != window.location.href) {
+    } else if (window.location.href.split('?')[0] === "http://localhost:3000/play" && oldHref !== window.location.href) {
       this.play(window.location.href);
       oldHref = window.location.href;
     }
-  }
+  };
 
   render() {
-    this.move();
+    this.move()
     return (
       <div className="App">
-        <div dangerouslySetInnerHTML={{ __html: template({cells: this.state.cells}) }} />
+        <div dangerouslySetInnerHTML={{ __html: this.state.template({cells: this.state.cells}) }} />
       </div>
-    );
-  }
-}
+    )
+  };
+};
 
 export default App;
